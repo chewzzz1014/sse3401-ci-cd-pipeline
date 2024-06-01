f6c0d722134e65f967b6378282b070d1635a2569
chewzzz <chewziqing@gmail.com>
2024-06-01 14:56:49 +0800

    add integration test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sse3401_ci_cd_pipeline/main.dart';
+import 'package:lab5/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
