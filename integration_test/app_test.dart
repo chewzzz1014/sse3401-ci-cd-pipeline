3eefe2bb15210f14820556479b5cff7dc0863320
chewzzz <chewziqing@gmail.com>
2024-06-01 15:55:33 +0800

    group integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,40 +5,46 @@ import 'package:lab5/otp/otp_main.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
-  testWidgets('OTPGetCode widget test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(
-        body: OTPGetCode(),
-      ),
-    ));
+  group('App test', () {
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-    // Verify if the widget is rendered
-    expect(find.byType(OTPGetCode), findsOneWidget);
+    testWidgets('OTPGetCode widget test', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: OTPGetCode(),
+        ),
+      ));
 
-    // Verify if the initial state is correct
-    expect(find.text('Enter your mobile number to activate your account.'), findsOneWidget);
-    expect(find.text('+60'), findsOneWidget);
-    expect(find.byType(ElevatedButton), findsOneWidget);
-    expect(find.byType(Checkbox), findsOneWidget);
+      // Verify if the widget is rendered
+      expect(find.byType(OTPGetCode), findsOneWidget);
 
-    // Enter phone number into TextFormField
-    await tester.enterText(find.byType(TextFormField), '123456789');
+      // Verify if the initial state is correct
+      expect(find.text('Enter your mobile number to activate your account.'),
+          findsOneWidget);
+      expect(find.text('+60'), findsOneWidget);
+      expect(find.byType(ElevatedButton), findsOneWidget);
+      expect(find.byType(Checkbox), findsOneWidget);
 
-    // Tap the Checkbox
-    await tester.tap(find.byType(Checkbox));
-    await tester.pump();
+      // Enter phone number into TextFormField
+      await tester.enterText(find.byType(TextFormField), '123456789');
 
-    // Verify if Checkbox is checked
-    expect(tester.widget<Checkbox>(find.byType(Checkbox)).value, isTrue);
+      // Tap the Checkbox
+      await tester.tap(find.byType(Checkbox));
+      await tester.pump();
 
-    // Verify if the ElevatedButton is enabled after entering a phone number and checking the checkbox
-    expect(tester.widget<ElevatedButton>(find.byType(ElevatedButton)).enabled, isTrue);
+      // Verify if Checkbox is checked
+      expect(tester.widget<Checkbox>(find.byType(Checkbox)).value, isTrue);
 
-    // Tap the ElevatedButton
-    // await tester.tap(find.byType(ElevatedButton));
-    // await tester.pump();
+      // Verify if the ElevatedButton is enabled after entering a phone number and checking the checkbox
+      expect(tester.widget<ElevatedButton>(find.byType(ElevatedButton)).enabled,
+          isTrue);
 
-    // Verify if the navigation to OTPMain widget is triggered after tapping the ElevatedButton
-    // expect(find.byType(OTPMain), findsOneWidget);
+      // Tap the ElevatedButton
+      // await tester.tap(find.byType(ElevatedButton));
+      // await tester.pump();
+
+      // Verify if the navigation to OTPMain widget is triggered after tapping the ElevatedButton
+      // expect(find.byType(OTPMain), findsOneWidget);
+    });
   });
 }
